0c6844cfba8a5d4c3121115307ef70fc30709108
Matt Jennings <mattjennings44@gmail.com>
2019-05-09 19:57:10 -0600

    some linting

--- index.js
+++ index.js
@@ -1,22 +1,22 @@
-var objmap = require('object-map');
-var objkeysmap = require('object-keys-map');
-var deepEqual = require('deep-equal');
+var objmap = require("object-map");
+var objkeysmap = require("object-keys-map");
+var deepEqual = require("deep-equal");
 
-var extend = typeof Object.assign === 'function' ? Object.assign : require('node-extend');
+var extend =
+  typeof Object.assign === "function" ? Object.assign : require("node-extend");
 
 function addDesign(s) {
-  return '_design/' + s;
+  return "_design/" + s;
 }
 
 function normalizeDoc(doc, id) {
-
   function normalize(doc) {
     doc = extend({}, doc);
     Object.keys(doc).forEach(function(prop) {
       var type = typeof doc[prop];
-      if(type === 'object') {
+      if (type === "object") {
         doc[prop] = normalize(doc[prop]);
-      } else if(type === 'function') {
+      } else if (type === "function") {
         doc[prop] = doc[prop].toString();
       }
     });
@@ -30,36 +30,38 @@ function normalizeDoc(doc, id) {
 }
 
 function docEqual(local, remote) {
-  if(!remote) return false;
-  return deepEqual(local, remote, {strict: true});
+  if (!remote) return false;
+  return deepEqual(local, remote, { strict: true });
 }
 
-var pouchSeed = module.exports = function (db, design, cb) {
+var pouchSeed = (module.exports = function(db, design, cb) {
   if (!db || !design) {
-    throw new TypeError('`db` and `design` are required');
+    throw new TypeError("`db` and `design` are required");
   }
 
   var local = objmap(objkeysmap(design, addDesign), normalizeDoc);
 
-  var seedPromise = db.allDocs({ include_docs: true, keys: Object.keys(local) })
+  var seedPromise = db
+    .allDocs({ include_docs: true, keys: Object.keys(local) })
 
-    .then(function (docs) {
-
+    .then(function(docs) {
       var remote = {};
 
-      docs.rows.forEach(function (doc) {
+      docs.rows.forEach(function(doc) {
         if (doc.doc) {
           remote[doc.key] = doc.doc;
         }
       });
 
-      var update = Object.keys(local).filter(function(key) {
-        if(!remote[key]) return true;
-        local[key]._rev = remote[key]._rev;
-        return !docEqual(local[key], remote[key]);
-      }).map(function(key) {
-        return local[key];
-      });
+      var update = Object.keys(local)
+        .filter(function(key) {
+          if (!remote[key]) return true;
+          local[key]._rev = remote[key]._rev;
+          return !docEqual(local[key], remote[key]);
+        })
+        .map(function(key) {
+          return local[key];
+        });
 
       if (update.length > 0) {
         return db.bulkDocs({ docs: update });
@@ -68,13 +70,13 @@ var pouchSeed = module.exports = function (db, design, cb) {
       }
     })
     .then(function(result) {
-      if(typeof cb === 'function') {
+      if (typeof cb === "function") {
         cb(null, result);
       }
       return Promise.resolve(result);
     })
     .catch(function(err) {
-      if(typeof cb === 'function') {
+      if (typeof cb === "function") {
         cb(err, null);
       }
       console.log(err);
@@ -82,9 +84,8 @@ var pouchSeed = module.exports = function (db, design, cb) {
     });
 
   return seedPromise;
+});
 
-};
-
-if(typeof window === 'object') {
+if (typeof window === "object") {
   window.pouchSeed = pouchSeed;
 }
